c4727123b55e4c3a37b4271709f8cf325e56ddb9
Valerie Salivon <ValerieSalivon@gmail.com>
2019-07-27 12:49:01 +0300

    Streams, IOFiles, FileWritter, FileReader

---
+++ Reader.java
@@ -0,0 +1,38 @@
+package IO_Streams;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Reader {
+
+    public void readFile(String source) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(source));
+        String c;
+        while ((c = reader.readLine()) != null) {
+            System.out.println(c);
+        }
+    }
+
+    public List<Student> readObjects(String source) {
+        List<Student> students = new ArrayList<>();
+        try (ObjectInputStream in = new ObjectInputStream(Files.newInputStream(Paths.get(source)))) {
+            Boolean keepReading = true;
+            while (keepReading) {
+                Student student = (Student) in.readObject();
+                if (!"".equals(student.getName())) {
+                    students.add(student);
+                } else keepReading = false;
+            }
+        } catch (IOException e) {
+            System.out.println("Unable to open file " + source + "\nProgram terminates.");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Invalid object type");
+            e.printStackTrace();
+        }
+        return students;
+    }
+}
